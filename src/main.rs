840ef56c54c396f55eeea932ebc1479497ae5ca3
Karl Palsson <karlp@etactica.com>
2021-09-03 13:44:19 +0000

    switch up to 64Mhz pll.
    itm starts working. now we can start figuring out what's going wrong.

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,12 @@ use cortex_m::{
 };
 use cortex_m_rt::entry;
 use stm32wb_hal as hal;
-use hal::prelude::*;
-use hal::hal::digital::v2::{PinState, OutputPin};
+use hal::{
+    hal::digital::v2::{PinState, OutputPin},
+    flash::{Parts},
+    prelude::*,
+    rcc::{ApbDivider, Config, HDivider, HseDivider, PllConfig, PllSrc, Rcc, SysClkSrc, },
+};
 
 #[entry]
 fn main() -> ! {
@@ -25,10 +29,11 @@ fn main() -> ! {
 
     // Use default clock frequency of 4 MHz running from MSI
     let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
 
-    //let clocks = setup_clocks();
-    let clocks = hal::rcc::Clocks::default();
-    let mut delay_naiive = hal::delay::DelayCM::new(clocks);
+    let mut rcc = setup_clocks(rcc, flash);
+    //let clocks = hal::rcc::Clocks::default();
+    let mut delay_naiive = hal::delay::DelayCM::new(rcc.clocks);
 
     // On STM32WB55-NUCLEO a green LED is connected to the pin PB0
     let mut gpiob = dp.GPIOB.split(&mut rcc);
@@ -51,11 +56,6 @@ fn main() -> ! {
         // delay....
         delay_naiive.delay_ms(300 as u16);
 
-        // Ideally, in c, where the bit and results in a bool...
-        // ledg.set_state(i & 1);
-        // ledr.set_state(i & 2);
-        // ledb.set_state(i & 4);
-        // but I need a "PinState"
         ledr.set_state(PinState::from((i & 1) > 0));
         ledg.set_state(PinState::from((i & 2) > 0));
         ledb.set_state(PinState::from((i & 4) > 0));
@@ -63,5 +63,20 @@ fn main() -> ! {
     }
 }
 
-fn setup_clocks() {
+fn setup_clocks(rcc: Rcc, mut flash: Parts) -> Rcc {
+
+    let clock_config_hse32_pll64 = Config::new(SysClkSrc::Pll(PllSrc::Hse(HseDivider::NotDivided)))
+        .cpu1_hdiv(HDivider::NotDivided)
+        .cpu2_hdiv(HDivider::Div2)
+        .apb1_div(ApbDivider::NotDivided)
+        .apb2_div(ApbDivider::NotDivided)
+        .pll_cfg(PllConfig {
+            m: 2,
+            n: 12,
+            r: 3,
+            q: Some(4),
+            p: Some(3),
+        });
+    // rcc.apply_clock_config(clock_config, &mut dp.FLASH.constrain().acr)
+    rcc.apply_clock_config(clock_config_hse32_pll64, &mut flash.acr)
 }
