117bbf70409746bc95a9c9cdf6fed4eccdbd5f6e
Karl Palsson <karlp@etactica.com>
2021-09-13 10:01:31 +0000

    abort abort
    Tried to get a timer going, no luck.  giving up on rust. shelving this.

--- examples/button-rtic.rs
+++ examples/button-rtic.rs
@@ -31,7 +31,7 @@ impl StimExt for Stim {
 }
 
 
-#[rtic::app(device = hal::pac, peripherals = true)]
+#[rtic::app(device = hal::pac, peripherals = true, dispatchers = [LCD])]
 mod app {
     use cortex_m::{
         iprintln,
@@ -40,7 +40,7 @@ mod app {
     use stm32wb_hal as hal;
     use hal::{
         delay::DelayCM,
-        flash::{Parts},
+        flash,
         gpio::{
             Edge, ExtiPin, Input, Output, PullUp, PushPull,
             gpiob::{PBx, PB0, PB1, PB5},
@@ -51,8 +51,11 @@ mod app {
         prelude::*,
         rcc::{ApbDivider, Config, HDivider, HseDivider, PllConfig, PllSrc, Rcc, RtcClkSrc, RfWakeupClock, SysClkSrc, },
         stm32,
+        timer::{Event, Timer},
     };
     use crate::StimExt;
+    use hal::pac::{EXTI, SYSCFG, TIM16};
+    use stm32wb_hal::hal::digital::v2::InputPin;
 
     // I hate that these have to be differently typed...
     pub type Sw1 = PC4<Input<PullUp>>;
@@ -76,24 +79,45 @@ mod app {
         ledg: LedPB,
         #[lock_free]
         ledb: LedPB,
+        // dp: hal::stm32::Peripherals,
     }
 
     #[local]
     struct Local {
         delay: DelayCM,
+        buffer: &'static mut [u16;1024],
+        tim16_cnt: u32,
+        tim16: Timer<TIM16>,
     }
 
-    #[init]
+    #[init(local=[the_buffer: [u16; 1024] = [0; 1024]])]
     fn init(ctx: init::Context) -> (Shared, Local, init::Monotonics) {
 
         let mut cp: cortex_m::Peripherals = ctx.core;
-        let mut dp: hal::stm32::Peripherals = ctx.device;
+        let mut dp: stm32::Peripherals = ctx.device;
 
         let rcc = dp.RCC.constrain();
         let flash = dp.FLASH.constrain();
         let mut rcc = setup_clocks(rcc, flash);
 
         let delay_naiive = hal::delay::DelayCM::new(rcc.clocks);
+
+        let buffer: &'static mut [u16; 1024] = ctx.local.the_buffer;
+
+        // Tim16
+        let mut timer = Timer::tim16(dp.TIM16, 5.hz(), rcc.clocks, &mut rcc);
+        timer.listen(Event::TimeOut);
+        // no, this is for _masking_ them, not unmasking them.
+        // // ALSO syscfg!  gpios handle this internally, via the "EXTI" which is not correct, it's SYSCFG!
+        // // fn enable_interrupt(&mut self, exti: &mut EXTI) {
+        // //     exti.imr1.modify(|r, w| unsafe { w.bits(r.bits() | (1 << self.i)) });
+        // // }
+        // let exti:&mut ExtiExt = &mut dp.EXTI;
+        // // bit 14 is for tim16.. fuck this shit.
+        // exti.imr1.modify(|r,w| unsafe { w.bits(r.bits() | (1<<14))});
+
+        unsafe { NVIC::unmask(hal::stm32::Interrupt::TIM1_UP) };
+
 
         let stim = &mut cp.ITM.stim;
         iprintln!(&mut stim[0], "RTIC's alive clock is {:?}", rcc.clocks.sysclk());
@@ -104,8 +128,14 @@ mod app {
         let ledg = gpiob.pb0.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
         let ledr = gpiob.pb1.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
         let ledb = gpiob.pb5.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
-        // exit 15_10 pc 13 if sb48 is on,
-        //let mut sw1 = gpioc.pc13.into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
+
+        // let sw1 = setup_button(gpioc, gpioc.pc4, stm32::Interrupt::EXTI4, &mut dp.SYSCFG, &mut dp.EXTI);
+        // let sw1 = setup_button_C(gpioc, gpioc.pc4, stm32::Interrupt::EXTI4, &mut dp.SYSCFG, &mut dp.EXTI);
+        // let sw2 = setup_button(gpiod, gpiod.pd0, stm32::Interrupt::EXTI0, &mut dp.SYSCFG, &mut dp.EXTI);
+        // let sw3 = setup_button(gpiod, gpiod.pd1, stm32::Interrupt::EXTI1, &mut dp.SYSCFG, &mut dp.EXTI);
+
+        // // exit 15_10 pc 13 if sb48 is on,
+        let mut sw1 = gpioc.pc13.into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
         // exti 4, pc4 if sb48 is off and sb47 is on (default)
         let mut sw1 = gpioc.pc4.into_pull_up_input(&mut gpioc.moder, &mut gpioc.pupdr);
         // exti 0
@@ -129,39 +159,73 @@ mod app {
             NVIC::unmask(stm32::Interrupt::EXTI1);
             NVIC::unmask(stm32::Interrupt::EXTI4);
         }
+        // setup_adc_task();
+
+
+        my_adc_task::spawn().unwrap();
 
         (Shared{
             sw1, sw2, sw3,
             // stim,
             ledr: ledr.downgrade(), ledg: ledg.downgrade(), ledb: ledb.downgrade(),
+            // dp,
         },
-         Local{ delay: delay_naiive},
+         Local{
+             delay: delay_naiive,
+             buffer,
+             tim16: timer,
+             tim16_cnt: 0,
+         },
          init::Monotonics())
     }
 
-    #[idle(local=[delay], shared=[ledr, ledg, ledb])]
+    #[idle(local=[], shared=[])]
     fn idle(ctx: idle::Context) -> ! {
         let stim = unsafe {&mut cortex_m::Peripherals::steal().ITM.stim};
         iprintln!(&mut stim[0], "idle task entered...");
 
+        // This task only handles going to sleep properly
+        loop {
+        }
+    }
+
+    #[task(local = [delay, buffer], shared = [ ledr, ledg ])]
+    fn my_adc_task(ctx: my_adc_task::Context) {
+        let stim = unsafe {&mut cortex_m::Peripherals::steal().ITM.stim};
+        iprintln!(&mut stim[0], "Started ADC task");
+
+        // let dp = ctx.shared.dp;
+
+
+
+        //
+        // let mut adc = ADC::new(
+        //     pac.ADC1,
+        //     pac.ADC_COMMON,
+        //     &mut rcc.ahb2,
+        //     &mut rcc.ccipr,
+        //     &mut delay,
+        // );
+        //
+
+
+
         let mut i = 0;
         loop {
-            iprintln!(&mut stim[0], "loopy da loop: {}", i);
+            i += 1;
+            iprintln!(&mut stim[0], "ADC loopy da loop: {}", i);
             stim[1].write_u16b(i);
             stim[2].write_u16b(i*2);
-            i += 1;
             ctx.local.delay.delay_ms(300 as u16);
 
             // both work, one way gives you completion in intellij
             let ledr: &mut LedPB = ctx.shared.ledr;
             ledr.set_state(PinState::from((i & 1) > 0));
             ctx.shared.ledg.set_state(PinState::from((i & 2) > 0));
-            ctx.shared.ledb.set_state(PinState::from((i & 4) > 0));
         }
     }
 
-
-    fn setup_clocks(rcc: Rcc, mut flash: Parts) -> Rcc {
+    fn setup_clocks(rcc: Rcc, mut flash: flash::Parts) -> Rcc {
         // 32MHz is enough for us for now, and uses less power.
         let clock_config_hse32_nopll = Config::new(SysClkSrc::HseSys(HseDivider::NotDivided))
             .cpu1_hdiv(HDivider::NotDivided)
@@ -173,6 +237,28 @@ mod app {
             ;
         rcc.apply_clock_config(clock_config_hse32_nopll, &mut flash.acr)
     }
+
+    // fn setup_button(port, pin, nvic: stm32::Interrupt, syscfg: &mut SYSCFG, exti: &mut EXTI) -> some() {
+    //     let mut sw = pin.into_pull_up_input(&mut port.moder, &mut port.pupdr);
+    //     sw.make_interrupt_source(syscfg);
+    //     sw.enable_interrupt(exti);
+    //     sw.trigger_on_edge(exti, Edge::Falling);
+    //     unsafe {
+    //         NVIC::unmask(nvic);
+    //     }
+    //     sw
+    // }
+    // fn setup_button_C<E>(port: &mut hal::gpio::gpioc::Parts, &mut pin: hal::gpio::gpioc::PCx<InputPin<Error=E>>, nvic: stm32::Interrupt, syscfg: &mut SYSCFG, exti: &mut EXTI)  {
+    //     let mut sw = pin.into_pull_up_input(&mut port.moder, &mut port.pupdr);
+    //     sw.make_interrupt_source(syscfg);
+    //     sw.enable_interrupt(exti);
+    //     sw.trigger_on_edge(exti, Edge::Falling);
+    //     unsafe {
+    //         NVIC::unmask(nvic);
+    //     }
+    //     sw
+    // }
+
 
     #[task(binds = EXTI4, shared = [sw1])]
     fn handle_sw1(ctx: handle_sw1::Context) {
@@ -200,6 +286,17 @@ mod app {
         //ctx.shared.sw3.lock(|b| b.clear_interrupt_pending_bit());
         ctx.shared.sw3.clear_interrupt_pending_bit();
     }
+
+    // PAC defines it as TIM1_UP, not TIM1_UP_TIM16....
+    #[task(binds = TIM1_UP, shared = [ledb], local = [tim16, tim16_cnt])]
+    fn handle_tim16(ctx: handle_tim16::Context) {
+        ctx.local.tim16.clear_update_interrupt_flag();
+        let stim = unsafe {&mut cortex_m::Peripherals::steal().ITM.stim};
+        iprintln!(&mut stim[0], "TIM16!");
+        *ctx.local.tim16_cnt += 1;
+        ctx.shared.ledb.set_state(PinState::from(*ctx.local.tim16_cnt % 2 == 0 ));
+    }
+
 }
 
 #[exception]
